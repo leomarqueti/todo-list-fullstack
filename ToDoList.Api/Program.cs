401a0f6cd4a6c27a79834ec743d29ba37e0bd716
Leonardo Sato <domme@DESKTOP-BDV1AKL>
2026-01-31 23:30:12 -0300

    Adicionado CORS

--- ToDoList.Api/Program.cs
+++ ToDoList.Api/Program.cs
@@ -6,6 +6,18 @@ using ToDoList.Domain.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// 1. No Builder (Services)
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("MinhaAppReact", policy =>
+    {
+        policy.WithOrigins("http://localhost:3000") // URL do seu front
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+
 var connectionString = builder.Configuration.GetConnectionString("Connection");
 
 builder.Services.AddDbContext<ToDoListDbContext>(options =>
@@ -22,6 +34,12 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseRouting();
+
+app.UseCors("MinhaAppReact"); // Aplica a política que você criou acima
+
+app.UseAuthorization();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
